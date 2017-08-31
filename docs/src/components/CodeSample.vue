a86b5ccd074920bd2bb6d7f0187beb7e8ec7cb87
Burak Karakan <burak.karakan@gmail.com>
2017-08-31 14:48:56 +0300

    Added docs to the subfolder.

---
+++ docs/src/components/CodeSample.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <pre><code class="javascript">{{code}}</code></pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CodeSample',
+  data() {
+    return {
+      code: `<info-card
+  :frontType="front.type"
+  :frontTitle="front.title"
+  :frontData="front.graphData"
+  :backTitle="back.title"
+  :backData="back.message"/>`,
+    };
+  },
+};
+</script>
+
+<style>
+code {
+  font-size: 16px;
+}
+</style>
---
+++ docs/src/components/DemoCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <info-card id="info-card"
+    :frontType="'graph'"
+    :frontTitle="front.title"
+    :frontData="front.graphData"
+    :backTitle="back.title"
+    :backData="back.message"/>
+</template>
+
+<script>
+import InfoCard from 'vue-info-card';
+
+export default {
+  name: 'app',
+  components: {
+    InfoCard,
+  },
+  data() {
+    return {
+      front: {
+        title: 'Some Rates',
+        graphData: [3, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
+      },
+      back: {
+        title: 'Monthly Summary',
+        message: '<i>Do you know why people like violence? It is because it feels good. Humans find violence deeply satisfying. But remove the satisfaction, and the act becomes hollow.</i><br><span class="u-pull-right">— Alan Turing</span>',
+      },
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
