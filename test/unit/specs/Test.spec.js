9763f3c022e0057454e0ad5aa4883fbf85dcceb8
Burak Karakan <burak.karakan@gmail.com>
2017-09-01 18:26:55 +0300

    Fixed test setup for the whole component.

---
+++ test/unit/specs/Test.spec.js
@@ -0,0 +1,7 @@
+describe('AppBody', () => {
+  // Check if it renders the button text properly for Google AdWords.
+  it('renders correct text for given title', () => {
+    expect(true).to.equal(true);
+  });
+});
+
