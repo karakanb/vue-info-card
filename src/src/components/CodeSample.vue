1fd615ca86075bf5095920dbcd4483b5d2152787
Burak Karakan <burak.karakan@gmail.com>
2019-09-19 14:20:11 +0200

    Updated examples to use v0.1.3

--- src/src/components/CodeSample.vue
+++ src/src/components/CodeSample.vue
@@ -10,11 +10,13 @@ export default {
   data() {
     return {
       code: `<info-card
-  :frontType="front.type"
-  :frontTitle="front.title"
-  :frontData="front.graphData"
-  :backTitle="back.title"
-  :backData="back.message"></info-card>`,
+    id="info-card"
+    frontType="graph"
+    frontTitle="Some Numbers"
+    :frontData="[3, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]"
+    backTitle="Quote of the Day"
+    backData='<i>Do you know why people like violence? It is because it feels good. Humans find violence deeply satisfying. But remove the satisfaction, and the act becomes hollow.</i><br><span class="u-pull-right">— Alan Turing</span>'
+  ></info-card>`,
     };
   },
 };
--- src/src/components/DemoCard.vue
+++ src/src/components/DemoCard.vue
@@ -1,10 +1,12 @@
 <template>
-  <info-card id="info-card"
-    :frontType="'graph'"
-    :frontTitle="front.title"
-    :frontData="front.graphData"
-    :backTitle="back.title"
-    :backData="back.message"/>
+  <info-card
+    id="info-card"
+    frontType="graph"
+    frontTitle="Some Numbers"
+    :frontData="[3, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0]"
+    backTitle="Quote of the Day"
+    backData='<i>Do you know why people like violence? It is because it feels good. Humans find violence deeply satisfying. But remove the satisfaction, and the act becomes hollow.</i><br><span class="u-pull-right">— Alan Turing</span>'
+  ></info-card>
 </template>
 
 <script>
@@ -23,7 +25,8 @@ export default {
       },
       back: {
         title: 'Monthly Summary',
-        message: '<i>Do you know why people like violence? It is because it feels good. Humans find violence deeply satisfying. But remove the satisfaction, and the act becomes hollow.</i><br><span class="u-pull-right">— Alan Turing</span>',
+        message:
+          '<i>Do you know why people like violence? It is because it feels good. Humans find violence deeply satisfying. But remove the satisfaction, and the act becomes hollow.</i><br><span class="u-pull-right">— Alan Turing</span>',
       },
     };
   },
@@ -31,5 +34,4 @@ export default {
 </script>
 
 <style>
-
 </style>
